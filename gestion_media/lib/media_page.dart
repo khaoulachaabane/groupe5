10dbfc61937d5ab9f104d941bed02d2cd9c04280
salma1bourakkadi <salma.bourakkadi@etu.imt-nord-europe.fr>
2024-02-11 22:08:03 +0100

    v10

--- gestion_media/lib/media_page.dart
+++ gestion_media/lib/media_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class MediaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: [
-          // Contenu de la page d'accueil
+          // Contenu de la page Media
         ],
       ),
     );
