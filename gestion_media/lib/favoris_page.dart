13612941d4d41eb90f29cf02358ed26894f3d277
salma1bourakkadi <salma.bourakkadi@etu.imt-nord-europe.fr>
2024-02-12 18:33:05 +0100

    vn-1

---
+++ gestion_media/lib/favoris_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavorisPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favoris'),
+      ),
+      body: Center(
+        child: Text('Liste des favoris ici'),
+      ),
+    );
+  }
+}
--- gestion_media/lib/films_details_page.dart
+++ gestion_media/lib/films_details_page.dart
@@ -8,6 +8,61 @@ class FilmDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Liste des films avec leurs détails
+    List<Map<String, String>> films = [
+      {
+        'title': 'The Hunger Games',
+        'description': 'Description de The Hunger Games',
+        'author': 'Suzanne Collins',
+        'country': 'États-Unis',
+        'genre': 'Science-fiction, Dystopie',
+        'language': 'Anglais',
+        'releaseDate': '2012',
+        'location': 'Panem',
+      },
+      {
+        'title': 'Catching Fire',
+        'description': 'Description de Catching Fire',
+        'author': 'Suzanne Collins',
+        'country': 'États-Unis',
+        'genre': 'Science-fiction, Dystopie',
+        'language': 'Anglais',
+        'releaseDate': '2013',
+        'location': 'Panem',
+      },
+      {
+        'title': 'Mockingjay Pt.1',
+        'description': 'Description de MOckingjay Pt.1',
+        'author': 'Suzanne Collins',
+        'country': 'États-Unis',
+        'genre': 'Science-fiction, Dystopie',
+        'language': 'Anglais',
+        'releaseDate': '2014',
+        'location': 'District 13',
+      },
+      {
+        'title': 'Mockingjay Pt.1',
+        'description': 'Description de MOckingjay Pt.1',
+        'author': 'Suzanne Collins',
+        'country': 'États-Unis',
+        'genre': 'Science-fiction, Dystopie',
+        'language': 'Anglais',
+        'releaseDate': '2015',
+        'location': 'Capitole',
+      },
+      // Ajoutez les détails des deux autres films de la même manière
+      // ...
+    ];
+
+    // Trouvez les détails du film actuel dans la liste
+    Map<String, String>? currentFilm;
+    for (var film in films) {
+      if (film['title'] == title) {
+        currentFilm = film;
+        break;
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -17,62 +72,60 @@ class FilmDetailsPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset('assets/images/films/thehungergames.png', height: 200.0, width: 200.0), // Ajout de l'image
+            Image.asset('assets/images/films/${title.toLowerCase().replaceAll(" ", "")}.png', height: 200.0, width: 200.0),
             SizedBox(height: 16.0),
             Text('Détails du film:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             SizedBox(height: 8.0),
             Table(
               columnWidths: {
-                0: FlexColumnWidth(1.5), // Ajustez la largeur des colonnes selon vos besoins
+                0: FlexColumnWidth(1.5),
                 1: FlexColumnWidth(2.0),
               },
               children: [
                 TableRow(
                   children: [
                     TableCell(child: Text('Auteur:')),
-                    TableCell(child: Text('Suzanne Collins')),
+                    TableCell(child: Text(currentFilm?['author'] ?? '')),
                   ],
                 ),
                 TableRow(
                   children: [
                     TableCell(child: Text('Pays:')),
-                    TableCell(child: Text('États-Unis')),
+                    TableCell(child: Text(currentFilm?['country'] ?? '')),
                   ],
                 ),
                 TableRow(
                   children: [
                     TableCell(child: Text('Genre:')),
-                    TableCell(child: Text('Science-fiction, Dystopie')),
+                    TableCell(child: Text(currentFilm?['genre'] ?? '')),
                   ],
                 ),
                 TableRow(
                   children: [
                     TableCell(child: Text('Langue:')),
-                    TableCell(child: Text('Anglais')),
+                    TableCell(child: Text(currentFilm?['language'] ?? '')),
                   ],
                 ),
                 TableRow(
                   children: [
                     TableCell(child: Text('Date de parution:')),
-                    TableCell(child: Text('2012')),
+                    TableCell(child: Text(currentFilm?['releaseDate'] ?? '')),
                   ],
                 ),
                 TableRow(
                   children: [
                     TableCell(child: Text('Lieu:')),
-                    TableCell(child: Text('Panem')),
+                    TableCell(child: Text(currentFilm?['location'] ?? '')),
                   ],
                 ),
               ],
             ),
             SizedBox(height: 16.0),
             Text('Description:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            Text(description),
+            Text(currentFilm?['description'] ?? ''),
           ],
         ),
       ),
     );
   }
 }
-
-
